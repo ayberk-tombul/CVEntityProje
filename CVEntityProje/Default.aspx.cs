92f580d9af584f964c428a582daa6d0705b154a6
AYBERK TOMBUL <ayberktombul@gmail.com>
2023-03-13 19:18:13 +0300

    Update.

--- CVEntityProje/Default.aspx.cs
+++ CVEntityProje/Default.aspx.cs
@@ -9,8 +9,15 @@ namespace CVEntityProje
 {
     public partial class Default : System.Web.UI.Page
     {
+        DBCVENTITYEntities db = new DBCVENTITYEntities();
         protected void Page_Load(object sender, EventArgs e)
         {
+            Repeater1.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater1.DataBind();
+            Repeater2.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater2.DataBind();
+            Repeater3.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater3.DataBind();
 
         }
     }
--- CVEntityProje/Default.aspx.designer.cs
+++ CVEntityProje/Default.aspx.designer.cs
@@ -13,5 +13,32 @@ namespace CVEntityProje
 
     public partial class Default
     {
+
+        /// <summary>
+        /// Repeater1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
+
+        /// <summary>
+        /// Repeater2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater2;
+
+        /// <summary>
+        /// Repeater3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater3;
     }
 }
