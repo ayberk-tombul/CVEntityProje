12e258ee613978ff386cd18c653a491da60d6ded
AYBERK TOMBUL <ayberktombul@gmail.com>
2023-03-17 17:36:20 +0300

    Update.

--- CVEntityProje/Default.aspx.cs
+++ CVEntityProje/Default.aspx.cs
@@ -18,6 +18,20 @@ namespace CVEntityProje
             Repeater2.DataBind();
             Repeater3.DataSource = db.TBLHAKKIMDA.ToList();
             Repeater3.DataBind();
+            Repeater4.DataSource = db.TBLYETENEKLER.ToList();
+            Repeater4.DataBind();
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TBLILETISIM t = new TBLILETISIM();
+            t.ADSOYAD = TextBox1.Text;
+            t.MAIL = TextBox2.Text;
+            t.KONU = TextBox3.Text;
+            t.MESAJ= TextBox4.Text;
+            db.TBLILETISIM.Add(t);
+            db.SaveChanges();
 
         }
     }
